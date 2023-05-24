7adff1772784d9a80af8f69ac8dc90e96e172407
mohit <connectwithlastdev@gmail.com>
2023-05-24 11:33:36 +0530

    setup mongodb databse

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app=require('./app')
 const dotenv=require('dotenv')
+const connectDatabase=require('./config/database')
 
 // config
-dotenv.config({path:'backend/config/config.env'})
+dotenv.config({path:'backend/config/updatedconfig.env'})
+
+// database
+connectDatabase()
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on http://localhost:${process.env.PORT}`);
 })
+
