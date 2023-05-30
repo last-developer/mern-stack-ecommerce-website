e076632156577fefc5a57fdc81b513020be2e8ea
mohit <connectwithlastdev@gmail.com>
2023-05-30 15:41:43 +0530

    finish register a user controller & route

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,10 @@ app.use(express.json())
 
 // route imports
 const product=require('./routes/productRoute')
+const user=require('./routes/userRoute')
+
 app.use('/api/v1',product)
+app.use('/api/v1',user)
 
 // error middleware
 app.use(errorMiddleware)
---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/userModel')
+const ErrorHander = require('../utils/errorhander')
+const catchAsyncErrors = require('../middleware/catchAsyncErrors')
+
+// Register a user
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+        name, email, password,
+        avatar: {
+            public_id: "im fake id",
+            url: "fakeurl"
+        }
+    })
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
