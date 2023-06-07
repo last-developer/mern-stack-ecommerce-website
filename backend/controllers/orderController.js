51507d6f4c4fc046f84daa1096f21208995f6c06
mohit <connectwithlastdev@gmail.com>
2023-06-07 13:18:55 +0530

    finish controller for get order details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -16,3 +16,28 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
         order
     })
 })
+
+// get single order
+exports.getsingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (!order) {
+        return next(new ErrorHander('Order not found', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+
+// get logged in user order details
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id })
+
+    res.status(200).json({
+        success: true,
+        orders
+    });
+});
+
