460062f9716db75c90c75785383177dcc001c577
mohit <connectwithlastdev@gmail.com>
2023-06-09 15:42:19 +0530

    setup basic router from router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,11 +12,13 @@ function App() {
 
   return (
     <>
+      <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/'  element={<Home />}/>
+      </Routes>
       <Footer />
-
-
+    </BrowserRouter>
     </>
   )
 }
