a1aa014315a8e04f963404884d6e44ba5f1ce4a9
mohit <connectwithlastdev@gmail.com>
2023-06-10 12:54:49 +0530

    finish loader component

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import './Home.css'
 import ProductCard from './ProductCard'
 import MetaData from '../Layout/MetaData'
 import { getProduct } from '../../actions/ProductAction'
+import Loader from '../Layout/Loader/Loader';
 
 
 const Home = () => {
@@ -23,10 +24,10 @@ const Home = () => {
                         <a href="#">Electronics</a>
                     </li>
                     <li>
-                        <a href="#">Beauty</a>
+                        <a href="#">Fashion</a>
                     </li>
                     <li>
-                        <a href="#">Fashion</a>
+                        <a href="#">Beauty</a>
                     </li>
                     <li>
                         <a href="#">Decor</a>
@@ -41,9 +42,14 @@ const Home = () => {
                 <h2>Featured Products</h2>
             </div>
 
-            <div className="featured-products padding">
-                {products && products.map((product) => <ProductCard product={product} />)}
-            </div>
+            {loading ? <Loader /> :
+                (
+                    <div className="featured-products padding">
+                        {products && products.map((product) => <ProductCard product={product} />)}
+                    </div>
+                )
+            }
+
         </>
     )
 }
