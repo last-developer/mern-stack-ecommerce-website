f775ccba4b9ab229fc5e1b0aab21896eae34fa70
mohit <connectwithlastdev@gmail.com>
2023-06-14 21:57:18 +0530

    make ReviewCard component

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -8,6 +8,7 @@ import Loader from '../Layout/Loader/Loader';
 
 import { useParams } from 'react-router-dom';
 import { Rating } from '@mui/material';
+import ReviewCard from './ReviewCard';
 
 const ProductDetails = () => {
     const dispatch = useDispatch();
@@ -28,66 +29,85 @@ const ProductDetails = () => {
 
     return (
         <>
-            <div className="ProductDetails">
-                <div>
-                    <Carousel>
-                        {product.images &&
-                            product.images.map((item, i) => (
-                                <img
-                                    className="CarouselImage"
-                                    key={i}
-                                    src={item.url}
-                                    alt={`${i} Slide`}
-                                />
-                            ))}
-                    </Carousel>
-                </div>
+            {loading ? (<Loader />) : (
+                <>
+                    <div className="ProductDetails">
+                        <div>
+                            <Carousel>
+                                {product.images &&
+                                    product.images.map((item, i) => (
+                                        <img
+                                            className="CarouselImage"
+                                            key={i}
+                                            src={item.url}
+                                            alt={`${i} Slide`}
+                                        />
+                                    ))}
+                            </Carousel>
+                        </div>
 
-                <div>
-                    <div className="detailsBlock-1">
-                        <h2>{product.name}</h2>
-                        <p>Product # {product._id}</p>
-                    </div>
-                    <div className="detailsBlock-2">
-                        <Rating {...options} />
-                        <span className="detailsBlock-2-span">
-                            {" "}
-                            ({product.numOfReviews} Reviews)
-                        </span>
-                    </div>
-                    <div className="detailsBlock-3">
-                        <h1>{`₹${product.price}`}</h1>
-                        {/* <div className="detailsBlock-3-1"> */}
-                        {/* <div className="detailsBlock-3-1-1">
+                        <div>
+                            <div className="detailsBlock-1">
+                                <h2>{product.name}</h2>
+                                <p>Product # {product._id}</p>
+                            </div>
+                            <div className="detailsBlock-2">
+                                <Rating {...options} />
+                                <span className="detailsBlock-2-span">
+                                    {" "}
+                                    ({product.numOfReviews} Reviews)
+                                </span>
+                            </div>
+                            <div className="detailsBlock-3">
+                                <h1>{`₹${product.price}`}</h1>
+                                {/* <div className="detailsBlock-3-1"> */}
+                                {/* <div className="detailsBlock-3-1-1">
                     <button onClick={decreaseQuantity}>-</button>
                     <input readOnly type="number" value={quantity} />
                     <button onClick={increaseQuantity}>+</button>
                   </div> */}
-                        {/* <button
+                                {/* <button
                     disabled={product.Stock < 1 ? true : false}
                     onClick={addToCartHandler}
                   >
                     Add to Cart
                   </button> */}
-                        {/* </div> */}
+                                {/* </div> */}
 
-                        <p>
-                            Status:
-                            <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                                {product.Stock < 1 ? "OutOfStock" : "InStock"}
-                            </b>
-                        </p>
-                    </div>
+                                <p>
+                                    Status:
+                                    <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                                        {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                                    </b>
+                                </p>
+                            </div>
 
-                    <div className="detailsBlock-4">
-                        Description : <p>{product.description}</p>
-                    </div>
+                            <div className="detailsBlock-4">
+                                Description : <p>{product.description}</p>
+                            </div>
 
-                    {/* <button onClick={submitReviewToggle} className="submitReview">
+                            {/* <button onClick={submitReviewToggle} className="submitReview">
                 Submit Review
               </button> */}
-                </div>
-            </div>
+                        </div>
+                    </div>
+                    <h3 className="reviewsHeading">REVIEWS</h3>
+
+                    {product.reviews && product.reviews[0] ? (
+                        <div className="reviews">
+                            {product.reviews &&
+                                product.reviews.map((review) => (
+                                    <ReviewCard key={review._id} review={review} />
+                                ))}
+                        </div>
+                    ) : (
+                        <p className="noReviews">No Reviews Yet</p>
+                    )}
+
+
+
+                </>
+            )}
         </>
     );
 };
